63d54dc95747522bbb8d77f88997be36e58c86bf
Alexey Muranov <alexey.muranov@gmail.com>
2012-10-27 14:23:51 +0200

    Change CTT2013::Helpers
    Declare all but one methods in `CTT2013::Helpers` as `module_function`.  Add a module function `simple_fixed_url`, to be used in Sass helper functions.

--- helpers.rb
+++ helpers.rb
@@ -4,32 +4,39 @@ require 'i18n' # Internationalisation
 
 class CTT2013
   module Helpers
-    # Delegate translation helper to I18n
-    def t(*args); I18n.t(*args) end
-
-    # Delegate format localisation helper to I18n
-    def l(*args); I18n.l(*args) end
-
-    def text_from_boolean(bool, options = {})
-      bool ? "✓#{ t(:yes, options) }" : t(:no, options)
-    end
-
-    def capitalize_first_letter_of(str)
-      unless str.empty?
-        str = ActiveSupport::Multibyte::Chars.new(str)
-        str[0] = str[0].upcase
-        str.to_s
-      end
-    end
-
     # Patched url helper to work around missing sub URI part
     def fixed_url(path)
       url(path, false, false).sub(/\A\//, BASE_URL)
     end
 
-    # Parse page name to determine I18n localisation scope
-    def page_i18n_scope(page)
-      'pages.' + page.to_s.gsub(?/, ?.)
-    end
+    module_function
+
+      # Delegate translation helper to I18n
+      def t(*args); I18n.t(*args) end
+
+      # Delegate format localisation helper to I18n
+      def l(*args); I18n.l(*args) end
+
+      def text_from_boolean(bool, options = {})
+        bool ? "✓#{ t(:yes, options) }" : t(:no, options)
+      end
+
+      def capitalize_first_letter_of(str)
+        unless str.empty?
+          str = ActiveSupport::Multibyte::Chars.new(str)
+          str[0] = str[0].upcase
+          str.to_s
+        end
+      end
+
+      # Parse page name to determine I18n localisation scope
+      def page_i18n_scope(page)
+        'pages.' + page.to_s.gsub(?/, ?.)
+      end
+
+      def simple_fixed_url(path)
+        path.sub(/\A\//, BASE_URL)
+      end
+
   end
 end
