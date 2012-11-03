29f9c253ce11cbe74907aad49290093c18c4589f
Alexey Muranov <alexey.muranov@gmail.com>
2012-11-03 21:43:17 +0100

    Add an experimental PresentationHelpers module
    Add a module for presentation helper functions to simplify presenting model attributes in view templates.  One helper function is ready to use.

---
+++ presentation_helpers.rb
@@ -0,0 +1,84 @@
+# encoding: UTF-8 (magic comment)
+
+require 'i18n' # Internationalisation
+
+class CTT2013
+  module PresentationHelpers
+    module AbstractSmarterModelHelpers
+      def attribute_in_description_list(object, attribute, hints = {})
+        object_class = object.class
+
+        unless column_type = hints[:column_type]
+          # NOTE: assumes that `AttributeTypes` module is included
+          column_type = object_class.attribute_type(attribute)
+        end
+
+        human_attribute_name =
+          capitalize_first_letter_of(
+            object_class.human_attribute_name(attribute))
+
+        format_localisation_key =
+          case column_type
+          when :boolean
+            'formats.attribute_name?'
+          else
+            'formats.attribute_name:'
+          end
+
+        name_html =
+          t(format_localisation_key, :attribute => human_attribute_name)
+
+        value = object.public_send(attribute)
+
+        case column_type
+        when :boolean
+          html_class = :boolean
+          value_html = value.nil? ? nil : text_from_boolean(value)
+        when :date, :datetime
+          html_class = column_type
+          value_html = l value, :format => :custom
+        when :time
+          html_class = :time
+          value_html = l value, :format => :time_of_the_day
+        when :integer
+          html_class = :number
+          value_html = value
+        else
+          html_class = column_type
+          value_html = value
+        end
+
+        readonly = object_class.readonly_attributes.include?(attribute.to_s)
+        if readonly
+          value_html =
+            haml :'/helper_partials/abstract_smarter_model/_readonly_value_in_description_list',
+                 :locals => { :value => value_html }
+        end
+
+        haml :'/helper_partials/abstract_smarter_model/_attribute_in_description_list',
+             :locals => { :name       => name_html,
+                          :value      => value_html,
+                          :html_class => html_class }
+      end
+
+      def singular_association_in_description_list(object, attr)
+        fail # TODO: implement me
+      end
+
+      def collection_association_in_description_list(object, attr)
+        fail # TODO: implement me
+      end
+
+      def description_list(object, attributes)
+        fail # TODO: implement me
+      end
+    end
+
+    include AbstractSmarterModelHelpers
+
+    module ParticipantHelpers
+    end
+
+    include ParticipantHelpers
+  end
+end
