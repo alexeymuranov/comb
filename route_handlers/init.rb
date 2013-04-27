c3c22538da118f6de5517f5cd8b589eaf109fe41
Alexey Muranov <alexey.muranov@gmail.com>
2013-04-27 18:27:04 +0200

    Move some private methods from public to init

--- route_handlers/init.rb
+++ route_handlers/init.rb
@@ -59,6 +59,28 @@ class CTT2013 < Sinatra::Base
 
   private
 
+    def set_locale(locale)
+      I18n.locale = @locale = locale
+      @other_locales = LOCALES.reject { |l| l == @locale }
+    end
+
+    def set_page(page)
+      @page = page
+      @base_title = t('base_co_m_b_page_title')
+      @title =
+        "#{ @base_title } | #{ t(:title, :scope => page_i18n_scope(@page)) }"
+    end
+
+    def locale_from_user_input(suggested_locale)
+      suggested_locale = suggested_locale.to_s.downcase
+      LOCALES.find { |l| l.to_s == suggested_locale } || DEFAULT_LOCALE
+    end
+
+    def page_from_user_input(suggested_page)
+      suggested_page = suggested_page.to_s.downcase
+      PUBLIC_PAGES.find { |p| p.to_s == suggested_page } || COMB_HOME_PAGE
+    end
+
     def pagination_parameters_from_params
       view_parameters = params[:view] || {}
       per_page    = (view_parameters[:per_page] || 10).to_i
