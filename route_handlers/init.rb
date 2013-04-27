b0d6104d4a60f69b192b74d9e8fd30520e09ee12
Alexey Muranov <alexey.muranov@gmail.com>
2013-04-27 18:29:11 +0200

    Comment out unused methods

--- route_handlers/init.rb
+++ route_handlers/init.rb
@@ -71,15 +71,15 @@ class CTT2013 < Sinatra::Base
         "#{ @base_title } | #{ t(:title, :scope => page_i18n_scope(@page)) }"
     end
 
-    def locale_from_user_input(suggested_locale)
-      suggested_locale = suggested_locale.to_s.downcase
-      LOCALES.find { |l| l.to_s == suggested_locale } || DEFAULT_LOCALE
-    end
+    # def locale_from_user_input(suggested_locale)
+    #   suggested_locale = suggested_locale.to_s.downcase
+    #   LOCALES.find { |l| l.to_s == suggested_locale } || DEFAULT_LOCALE
+    # end
 
-    def page_from_user_input(suggested_page)
-      suggested_page = suggested_page.to_s.downcase
-      PUBLIC_PAGES.find { |p| p.to_s == suggested_page } || COMB_HOME_PAGE
-    end
+    # def page_from_user_input(suggested_page)
+    #   suggested_page = suggested_page.to_s.downcase
+    #   PUBLIC_PAGES.find { |p| p.to_s == suggested_page } || COMB_HOME_PAGE
+    # end
 
     def pagination_parameters_from_params
       view_parameters = params[:view] || {}
