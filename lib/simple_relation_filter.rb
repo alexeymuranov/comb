a29780c82ed57be559a864cf30d17f01134ba2d9
Alexey Muranov <alexey.muranov@gmail.com>
2013-05-05 13:42:12 +0200

    Use functional style in SimpleRelationFilter

--- lib/simple_relation_filter.rb
+++ lib/simple_relation_filter.rb
@@ -19,15 +19,13 @@ class SimpleRelationFilter
 
   def to_scope
 
-    model_scope = @model.scoped
-
     table_name = @model.table_name
 
-    @filtering_attributes.each do |attr|
+    @filtering_attributes.select { |attr|
+      @filtering_values.key?(attr)
+    }.reduce(@model.scoped) { |model_scope, attr|
 
       filtering_value = @filtering_values[attr]
-
-      next if filtering_value.nil?
 
       filtering_column_type = @model.attribute_type(attr)
 
@@ -37,99 +35,98 @@ class SimpleRelationFilter
       when :string
         case filtering_value
         when Set
-          model_scope =
-            model_scope.where("#{ column_sql } IN (?)", filtering_value)
+          model_scope.where("#{ column_sql } IN (?)", filtering_value)
         else
-          model_scope =
-            model_scope.where("#{ column_sql } LIKE ?", filtering_value)
+          model_scope.where("#{ column_sql } LIKE ?", filtering_value)
         end
 
       when :boolean
-        model_scope =
-          model_scope.where("#{ column_sql } = ?", filtering_value)
+        model_scope.where("#{ column_sql } = ?", filtering_value)
 
       when :integer
         case filtering_value
         when Hash
+          new_model_scope = model_scope
           if filtering_value.key?(:min)
             unless filtering_value[:min] == -Float::INFINITY
-              model_scope =
+              new_model_scope =
                 model_scope.where("#{ column_sql } >= ?", filtering_value[:min])
             end
           end
           if filtering_value.key?(:max)
             unless filtering_value[:max] == Float::INFINITY
-              model_scope =
+              new_model_scope =
                 model_scope.where("#{ column_sql } <= ?", filtering_value[:max])
             end
           end
+          new_model_scope
         when Set
-          model_scope =
-            model_scope.where("#{ column_sql } IN (?)", filtering_value)
+          model_scope.where("#{ column_sql } IN (?)", filtering_value)
         when Range
+          new_model_scope = model_scope
           unless filtering_value.first == -Float::INFINITY
-            model_scope =
+            new_model_scope =
               model_scope.where("#{ column_sql } >= ?", filtering_value.first)
           end
           unless filtering_value.last == Float::INFINITY
-            model_scope =
+            new_model_scope =
               if filtering_value.exclude_end?
                 model_scope.where("#{ column_sql } < ?", filtering_value.last)
               else
                 model_scope.where("#{ column_sql } <= ?", filtering_value.last)
               end
           end
+          new_model_scope
         else
-          model_scope =
-            model_scope.where("#{ column_sql } = ?", filtering_value)
+          model_scope.where("#{ column_sql } = ?", filtering_value)
         end
 
       when :date
         case filtering_value
         when Hash
+          new_model_scope = model_scope
           if filtering_value.key?(:from)
-            model_scope =
+            new_model_scope =
               model_scope.where("#{ column_sql } >= ?", filtering_value[:from])
           end
           if filtering_value.key?(:until)
-            model_scope =
+            new_model_scope =
               model_scope.where("#{ column_sql } <= ?", filtering_value[:until])
           end
+          new_model_scope
         when Set
-          model_scope =
-            model_scope.where("#{ column_sql } IN (?)", filtering_value)
+          model_scope.where("#{ column_sql } IN (?)", filtering_value)
         when Range
+          new_model_scope = model_scope
           unless filtering_value.first == -Float::INFINITY
-            model_scope =
+            new_model_scope =
               model_scope.where("#{ column_sql } >= ?", filtering_value.first)
           end
           unless filtering_value.last == Float::INFINITY
-            model_scope =
+            new_model_scope =
               if filtering_value.exclude_end?
                 model_scope.where("#{ column_sql } < ?", filtering_value.last)
               else
                 model_scope.where("#{ column_sql } <= ?", filtering_value.last)
               end
           end
+          new_model_scope
         else
-          model_scope =
-            model_scope.where("#{ column_sql } = ?", filtering_value)
+          model_scope.where("#{ column_sql } = ?", filtering_value)
         end
+      else
+        model_scope
       end
-    end
-
-    model_scope
+    }
   end
 
   def filtering_values_as_simple_nested_hash
 
-    filtering_simple_hash = {}
-
-    @filtering_attributes.each do |attr|
+    @filtering_attributes.select { |attr|
+      @filtering_values.key?(attr)
+    }.reduce({}) { |hash, attr|
 
       filtering_value = @filtering_values[attr]
-
-      next if filtering_value.nil?
 
       filtering_column_type = @model.attribute_type(attr)
 
@@ -137,69 +134,68 @@ class SimpleRelationFilter
       when :string
         case filtering_value
         when Set
-          filtering_simple_hash[attr] = filtering_value.to_a
+          hash[attr] = filtering_value.to_a
         else
-          filtering_simple_hash[attr] = filtering_value
+          hash[attr] = filtering_value
         end
 
       when :boolean
-        filtering_simple_hash[attr] = filtering_value
+        hash[attr] = filtering_value
 
       when :integer
         case filtering_value
         when Hash
-          filtering_simple_hash[attr] = {}
+          hash[attr] = {}
           if filtering_value.key?(:min)
             unless filtering_value[:min] == -Float::INFINITY
-              filtering_simple_hash[attr][:min] = filtering_value[:min]
+              hash[attr][:min] = filtering_value[:min]
             end
           end
           if filtering_value.key?(:max)
             unless filtering_value[:max] == Float::INFINITY
-              filtering_simple_hash[attr][:max] = filtering_value[:max]
+              hash[attr][:max] = filtering_value[:max]
             end
           end
         when Set
-          filtering_simple_hash[attr] = filtering_value.to_a
+          hash[attr] = filtering_value.to_a
         when Range
-          filtering_simple_hash[attr] = {}
+          hash[attr] = {}
           unless filtering_value.first == -Float::INFINITY
-            filtering_simple_hash[attr][:min] = filtering_value.first
+            hash[attr][:min] = filtering_value.first
           end
           unless filtering_value.last == Float::INFINITY
-            filtering_simple_hash[attr][:max] = filtering_value.last
+            hash[attr][:max] = filtering_value.last
           end
         else
-          filtering_simple_hash[attr] = filtering_value
+          hash[attr] = filtering_value
         end
 
       when :date
         case filtering_value
         when Hash
-          filtering_simple_hash[attr] = {}
+          hash[attr] = {}
           if filtering_value.key?(:from)
-            filtering_simple_hash[attr][:from] = filtering_value[:from]
+            hash[attr][:from] = filtering_value[:from]
           end
           if filtering_value.key?(:until)
-            filtering_simple_hash[attr][:until] = filtering_value[:until]
+            hash[attr][:until] = filtering_value[:until]
           end
         when Set
-          filtering_simple_hash[attr] = filtering_value.to_a
+          hash[attr] = filtering_value.to_a
         when Range
-          filtering_simple_hash[attr] = {}
+          hash[attr] = {}
           unless filtering_value.first == -Float::INFINITY
-            filtering_simple_hash[attr][:from] = filtering_value.first
+            hash[attr][:from] = filtering_value.first
           end
           unless filtering_value.last == Float::INFINITY
-            filtering_simple_hash[attr][:until] = filtering_value.last
+            hash[attr][:until] = filtering_value.last
           end
         else
-          filtering_simple_hash[attr] = filtering_value
+          hash[attr] = filtering_value
         end
       end
-    end
-
-    filtering_simple_hash
+      hash
+    }
   end
 end
 
