3ca729f423b46df4d29b4219dfa26e55bfe26923
Alexey Muranov <alexey.muranov@gmail.com>
2013-05-27 08:21:33 +0200

    Simplify cache control "before filters"

--- route_handlers/filters.rb
+++ route_handlers/filters.rb
@@ -2,20 +2,20 @@
 
 class CTT2013 < Sinatra::Base
 
-  # Cache control
-  before do
-    cache_control :public, :must_revalidate, :max_age => 60
-  end
-
-  before %r{/org/} do
-    cache_control :no_cache
-  end
-
   # Set locale
   before '/:loc/*' do |loc, path|
     if locale = LOCALE_FROM_URL_LOCALE_FRAGMENT["#{ loc }/"]
       set_locale(locale)
       request.path_info = "/#{ path }"
+    end
+  end
+
+  # Cache control
+  before '/*' do |path|
+    if path =~ %r{\Aorg/}
+      cache_control :no_cache
+    else
+      cache_control :public, :must_revalidate, :max_age => 60
     end
   end
 
