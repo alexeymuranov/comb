955ce686658f771fc183590ea1286a9cec51a733
Alexey Muranov <alexey.muranov@gmail.com>
2014-05-14 00:02:31 +0200

    move Application private methods to application.rb

--- application.rb
+++ application.rb
@@ -113,6 +113,41 @@ module CTT2013
     # Session-based flash
     register Sinatra::Flash
   end
+
+  # Private methods
+  Application.class_eval do
+    private
+
+    def set_locale(locale)
+      I18n.locale = @locale = locale
+      @other_locales = CTT2013::LOCALES - [@locale]
+    end
+
+    def locale
+      @locale || CTT2013::DEFAULT_LOCALE
+    end
+
+    def set_page(page)
+      @page = page
+      @base_title = t('base_co_m_b_page_title')
+      @title =
+        "#{ @base_title } | #{ t(:title, :scope => page_i18n_scope(@page)) }"
+    end
+
+    def page
+      @page
+    end
+
+    # def locale_from_user_input(suggested_locale)
+    #   suggested_locale = suggested_locale.to_s.downcase
+    #   CTT2013::LOCALES.find{|l| l.to_s == suggested_locale } || CTT2013::DEFAULT_LOCALE
+    # end
+
+    # def page_from_user_input(suggested_page)
+    #   suggested_page = suggested_page.to_s.downcase
+    #   PUBLIC_PAGES.find{|p| p.to_s == suggested_page } || COMB_HOME_PAGE
+    # end
+  end
 end
 
 CTT2013::Application.run! if __FILE__ == $0
--- route_handlers/all.rb
+++ route_handlers/all.rb
@@ -1,7 +1,6 @@
 # encoding: UTF-8 (magic comment)
 
 require_relative 'constants'
-require_relative 'private_methods'
 require_relative 'params_processing'
 require_relative 'filters'
 require_relative 'public'
