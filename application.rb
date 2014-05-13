7c6adb9ec7bf8e72a0ad426fcdee3283b482aa84
Alexey Muranov <alexey.muranov@gmail.com>
2014-05-13 16:23:33 +0200

    do not require rubygems and bundler in application

--- application.rb
+++ application.rb
@@ -3,10 +3,6 @@
 Encoding.default_external = Encoding::UTF_8
 # Encoding.default_internal = Encoding::UTF_8
 
-require 'rubygems'
-
-require 'bundler/setup'
-
 require 'set' # to use Set data structure
 
 require 'digest' # to hash passwords
